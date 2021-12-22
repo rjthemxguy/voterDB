69727c3d4ab68ca57a6eb89e3f6032956589b493
rjthemxguy <rjrseo@gmail.com>
2021-12-22 03:38:40 +0000

    new routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const {check, validationResult} = require('express-validator/check');
 
 
 
@@ -8,10 +9,25 @@ const User = require('../models/User');
 // @desc 	Register a user
 // @access	Public
 
-router.post('/', (reg,res) => {
+router.post('/',
+[
+	check('name','Name is required xxx').not().isEmpty(),
+	check('email', 'Please include a valid email').isEmail(),
+	check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+],
 
-res.send('Register a user');
-});
+(req,res) => {
+	const errors = validationResult(req);
+		if(!errors.isEmpty()) {
+			return res.status(400).json({errors:errors.array()});
+
+	
+		}
+		res.send("Passed");
+	}
+
+);
+
 
 module.exports = router;
 
